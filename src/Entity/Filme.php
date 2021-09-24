6247ba90a9bcc5c6dff45ce0eacceb68d6c56bb9
Felipe Silva <felipe.dacs@gmail.com>
2021-09-24 01:57:22 -0300

    :tv: add aula 4

---
+++ src/Entity/Filme.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+class Filme
+{
+    private $id;
+    private $titulo;
+    private $sinopse;
+    private $anoLancamento;
+    private $ultimaAtualizacao;
+}
+
